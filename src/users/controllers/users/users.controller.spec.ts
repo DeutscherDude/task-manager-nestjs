e770c6fd58f2d6df48e9f012e16839dec6fd7654
DeutscherDude <kolokius@gmail.com>
2022-07-07 20:01:29 +0200

    Users almost fully tested, remaining the user module test

--- src/users/controllers/users/users.controller.spec.ts
+++ src/users/controllers/users/users.controller.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { tokens } from '../../../utils/serviceTokens';
+import { UsersService } from '../../services/users/users.service';
 import { UsersController } from './users.controller';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UsersService;
   const user = [{
-    id: 1,
+    id: '62c6f411ae263423c849119e',
     name: 'John',
     email: 'ya@no.me',
     password: 'abcdefg',
@@ -18,27 +20,29 @@ describe('UsersController', () => {
         {
           provide: tokens.USERS_SERVICE,
           useValue: {
-            getUsers: jest.fn().mockImplementation(() => {
+            findAll: jest.fn().mockImplementation(() => {
               return user;
             }),
-            getUserById: jest.fn().mockImplementation((id: number) => {
+            findUserById: jest.fn().mockImplementation((id: string) => {
               return user.find(user => user.id === id);
             }),
             createUser: jest.fn().mockImplementation((createUserDto: any) => {
               return 'User created';
             }),
-            updateUser: jest.fn().mockImplementation((id: number) => {
+            updateUser: jest.fn().mockImplementation((id: string) => {
               return user.find(user => user.id === id);
             }),
-            deleteUserById: jest.fn().mockImplementation((id: number) => {
+            deleteUserById: jest.fn().mockImplementation((id: string) => {
               return 'User deleted';
             }),
+            login: jest.fn()
           }
         }
       ]
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get(tokens.USERS_SERVICE);
   });
 
   it('should be defined', () => {
@@ -56,7 +60,7 @@ describe('UsersController', () => {
 
     describe('getUserById', () => {
       it('should return a user given a valid id', async () => {
-        expect(await controller.getUserById(1)).toEqual(user[0]);
+        expect(await controller.getUserById('62c6f411ae263423c849119e')).toEqual(user[0]);
       });
     });
 
@@ -73,6 +77,7 @@ describe('UsersController', () => {
 
     describe('updateUser', () => {
       it('should update a User given valid id', async () => {
+        jest.spyOn(service, 'updateUser').mockResolvedValue(user[0] as any);
         expect(await controller.updateUser(1)).toEqual(user[0]);
       });
     });
@@ -82,6 +87,17 @@ describe('UsersController', () => {
         expect(await controller.deleteUserById(1)).toBe('User deleted');
       });
     });
+
+    describe('login', () => {
+      it('should return a user given valid credentials', async () => {
+        jest.spyOn(service, 'login').mockResolvedValue(user[0] as any);
+        expect(await controller.login({
+          email: 'test@test.com',
+          password: 'test'
+        })).toBe(user[0]);
+      })
+    })
+
   });
 });
 
